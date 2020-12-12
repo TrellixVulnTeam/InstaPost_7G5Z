50372ebc4952472db70a88e4c7c0c9600a795e20
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2020-12-12 00:36:06 -0700

    django setup done successfully and new settings added.

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insta_post.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instablog_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
