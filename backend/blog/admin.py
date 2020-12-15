80ce32552c1fb8f27d688cd08b7a365db2aadbff
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2020-12-15 11:04:44 -0700

    views added

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
+
 
 # Register your models here.
+class BlogPostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    exclude = ('slug',)
+    summernote_fields = ('content',)
+    # list_display_links=('id','title',) #if needed turn this into a class. Refer stack overflow
+    search_fields=('title',)
+    list_per_page = 25
+    summernote_fields = ('content',)
+
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
